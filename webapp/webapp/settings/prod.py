d92c995c8a513fafe42e9b72747f7b456d81a31d
Cameron Hyde <pluckthechicken@hotmail.co.uk>
2021-11-05 11:23:22 +1000

    Debugged for prod serve

--- webapp/webapp/settings/prod.py
+++ webapp/webapp/settings/prod.py
@@ -1,5 +1,6 @@
 """Settings for production."""
 
+import os
 from dotenv import load_dotenv
 
 from .base import *
@@ -9,7 +10,7 @@ load_dotenv()
 
 DEBUG = False
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-ALLOWED_HOSTS.append('site.usegalaxy.org')
+ALLOWED_HOSTS.append('usegalaxy-au.neoformit.com')
 
 # DATABASES = {
 #     # Probably want a proper production database e.g. postgres
