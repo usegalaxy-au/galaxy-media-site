48496ab9f521f390eeb34071636dd490c55bb080
Cameron Hyde <pluckthechicken@hotmail.co.uk>
2021-11-12 15:50:45 +1000

    Update link to data policy

--- webapp/webapp/settings/prod.py
+++ webapp/webapp/settings/prod.py
@@ -15,9 +15,9 @@ ALLOWED_HOSTS.append('usegalaxy-au.neoformit.com')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ga_site',
-        # 'USER': 'ga_site',
-        # 'PASSWORD': '',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
