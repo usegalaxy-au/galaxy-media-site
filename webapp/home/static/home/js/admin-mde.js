81a2f856aef456a56b8e70e3bf015fdb26e4270f
Cameron Hyde <chyde@neoformit.com>
2021-11-23 11:13:38 +1000

    Fix window load function

--- webapp/home/static/home/js/admin-mde.js
+++ webapp/home/static/home/js/admin-mde.js
@@ -1,7 +1,7 @@
 // Add markdown editor to the "body" field in django admin
 
 (function($) {
-  $(document).on('load', () => {
+  $(document).ready( () => {
     var simplemde = new SimpleMDE({
       element: document.getElementById("id_body")
     });
--- webapp/home/static/home/js/prettify.js
+++ webapp/home/static/home/js/prettify.js
@@ -1,7 +1,7 @@
 // Prettify JSON fields
 
 (function($) {
-  $(document).on('load', () => {
+  $(document).ready( () => {
     el = document.getElementById('id_address')
     el.value = JSON.stringify(JSON.parse(el.value), null, 4)
     el.setAttribute('style', 'font-family: monospace;')
