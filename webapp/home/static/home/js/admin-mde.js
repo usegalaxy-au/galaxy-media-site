ad5384d3ed51028911f4e3ae14c3f2a6468d104b
Cameron Hyde <chyde@neoformit.com>
2021-11-23 11:07:21 +1000

    Fix window load to allow multiple onload functions

--- webapp/home/static/home/js/admin-mde.js
+++ webapp/home/static/home/js/admin-mde.js
@@ -1,7 +1,9 @@
 // Add markdown editor to the "body" field in django admin
 
-window.onload = () => {
-  var simplemde = new SimpleMDE({
-    element: document.getElementById("id_body")
+(function($) {
+  $(document).on('load', () => {
+    var simplemde = new SimpleMDE({
+      element: document.getElementById("id_body")
+    });
   });
-};
+})(django.jQuery);
--- webapp/home/static/home/js/prettify.js
+++ webapp/home/static/home/js/prettify.js
@@ -1,7 +1,9 @@
 // Prettify JSON fields
 
-window.onload = () => {
-  el = document.getElementById('id_address')
-  el.value = JSON.stringify(JSON.parse(el.value), null, 4)
-  el.setAttribute('style', 'font-family: monospace;')
-}
+(function($) {
+  $(document).on('load', () => {
+    el = document.getElementById('id_address')
+    el.value = JSON.stringify(JSON.parse(el.value), null, 4)
+    el.setAttribute('style', 'font-family: monospace;')
+  });
+})(django.jQuery);
