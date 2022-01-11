a045f7ad861eae368640a031bf9d9347be4c5e34
Cameron Hyde <chyde@neoformit.com>
2022-01-11 11:54:18 +1000

    Remove old static server conf

--- server/README.md
+++
@@ -1,7 +0,0 @@
-# Server files for manual deployment
-
-**Stack:**
-- Ubuntu 20.04
-- Postgresql
-- Nginx
-- Gunicorn
--- server/gunicorn.py
+++
@@ -1,6 +0,0 @@
-workers = 4
-
-# Environment variables
-raw_env = [
-    "DJANGO_SETTINGS_MODULE=webapp.settings.prod"
-    ]
