8e807eea9613408ef213ea63230e5d2b43c52704
Cameron Hyde <chyde@neoformit.com>
2022-03-07 10:07:46 +1000

    Fix django-light module name

--- webapp/webapp/settings/base.py
+++ webapp/webapp/settings/base.py
@@ -44,7 +44,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'django-light',
+    'django_light',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
