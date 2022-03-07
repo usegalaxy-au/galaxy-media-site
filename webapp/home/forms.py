1e536e65aad3a51b78b1ec2daafc4810e02a9db2
Cameron Hyde <chyde@neoformit.com>
2022-03-07 10:05:05 +1000

    Update webforms

--- webapp/home/forms.py
+++ webapp/home/forms.py
@@ -48,16 +48,11 @@ class ResourceRequestForm(forms.Form):
     resource_name_version = forms.CharField()
     resource_url = forms.URLField(required=False)
     resource_justification = forms.CharField(required=False)
-    resource_research_count = forms.IntegerField(required=False)
-    resource_research_groups = forms.CharField(required=False)
 
     # Fields for tool
     tool_toolshed_available = forms.BooleanField(required=False)
     tool_toolshed_url = forms.URLField(required=False)
     tool_test_data = forms.BooleanField(required=False)
-
-    # Fields for dataset
-    dataset_contact_consent = forms.BooleanField(required=False)
 
     captcha = fields.ReCaptchaField()
 
@@ -82,28 +77,14 @@ class ResourceRequestForm(forms.Form):
 class QuotaRequestForm(forms.Form):
     """Form for requesting data quota."""
 
-    DURATION_MONTHS_CHOICES = (
-        ('1', '1 month'),
-        ('3', '3 months'),
-        ('6', '6 months'),
-    )
-
     name = forms.CharField()
     email = forms.EmailField()
-    institution = forms.CharField()
-    group_name = forms.CharField()
     start_date = forms.DateField()
-    duration = forms.ChoiceField(choices=DURATION_MONTHS_CHOICES)
-    disk_gb = forms.IntegerField(required=False)  # null = don't know
+    duration_months = forms.IntegerField()
+    disk_tb = forms.IntegerField(required=False)  # null = don't know
+    disk_tb_other = forms.IntegerField(required=False)
     description = forms.CharField()
-    organism = forms.CharField(required=False)
-    organism_other = forms.CharField(required=False)
-    technology = forms.CharField(required=False)
-    technology_other = forms.CharField(required=False)
-    file_type = forms.CharField(required=False)
-    file_type_other = forms.CharField(required=False)
-    max_samples = forms.IntegerField(required=False)
-    max_samples_other = forms.IntegerField(required=False)
+    captcha = fields.ReCaptchaField()
     accepted_terms = forms.BooleanField()
 
     def clean(self):
@@ -111,21 +92,18 @@ class QuotaRequestForm(forms.Form):
         data = super().clean()
         # These fields have an "other" option with text input
         OTHER_FIELDS = [
-            'organism',
-            'technology',
-            'file_type',
-            'max_samples',
+            'disk_tb',
         ]
-        # These values for "organism" link to the "other" text input too
-        ORGANISM_OTHER_VALUES = [
-            'bacteria',
-            'plant',
-        ]
-
         for field in OTHER_FIELDS:
             if not data.get(field):
                 # User may have selected the 'other' field and typed a value
                 if not data.get(f'{field}_other'):
+
+                    from pprint import pprint
+                    pprint(dir(self.fields))
+
+                    if not self.fields[field].required:
+                        continue
                     self.add_error(
                         field,
                         ValidationError('This field is required'))
@@ -134,12 +112,6 @@ class QuotaRequestForm(forms.Form):
                     data[field] = 'Other - ' + other_value
                 else:
                     data[field] = other_value
-
-        for value in ORGANISM_OTHER_VALUES:
-            if data.get('organism') == value:
-                data['organism'] = (
-                    f"{value.title()} - {data['organism_other']}")
-
         return data
 
     def dispatch(self):
