8d0b8fffdd43d47d6f7a9079cf53710bc065add1
Cameron Hyde <chyde@neoformit.com>
2022-03-21 10:42:45 +1000

    Update dev settings with GALAXY_SITE_SUFFIX

--- webapp/webapp/settings/dev.py
+++ webapp/webapp/settings/dev.py
@@ -6,10 +6,13 @@ from .base import *
 
 DEBUG = False
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') or "secretkey"
-GALAXY_SITE_NAME = 'Australia'  # Rendered as "Galaxy <GALAXY_SITE_NAME> Media"
 if os.environ.get('HOSTNAME'):
     HOSTNAME = os.environ.get('HOSTNAME')
     ALLOWED_HOSTS.append(HOSTNAME)
+
+# Rendered as "Galaxy <GALAXY_SITE_NAME> <GALAXY_SITE_SUFFIX>"
+GALAXY_SITE_NAME = 'Australia'
+GALAXY_SITE_SUFFIX = 'Media'
 
 # For posting tool update notifications to a Slack channel
 SLACK_API_URL = (
