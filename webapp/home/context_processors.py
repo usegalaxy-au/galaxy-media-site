98eac39f09f3b4a9ad1ca3b2163751aca8f67e2e
Cameron Hyde <chyde@neoformit.com>
2022-03-22 12:13:31 +1000

    Update template context processor to display site name properly

--- webapp/home/context_processors.py
+++ webapp/home/context_processors.py
@@ -7,5 +7,6 @@ def settings(request):
     """Include some settings variables."""
     return {
         'title': None,  # prevent variable not found templating error
-        'GALAXY_SITE_NAME': webapp_settings.HOSTNAME,
+        'GALAXY_SITE_NAME': webapp_settings.GALAXY_SITE_NAME,
+        'GALAXY_SITE_SUFFIX': webapp_settings.GALAXY_SITE_SUFFIX,
     }
