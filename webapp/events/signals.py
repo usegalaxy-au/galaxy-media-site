5ca9443b68a7894b5a7f9037c645877fbddcc5ab
Cameron Hyde <chyde@neoformit.com>
2022-12-29 09:45:50 +1000

    Media file cleanup with django-cleanup

--- webapp/events/signals.py
+++ webapp/events/signals.py
@@ -12,12 +12,11 @@ against double-firing (e.g. check if file exists before creating).
 Must be called in apps.py to work.
 """
 
-import os
 from django.dispatch import receiver
 from django.db.models.signals import (
     # pre_delete,
-    post_delete,
-    pre_save,
+    # post_delete,
+    # pre_save,
     post_save,
 )
 
@@ -29,28 +28,3 @@ def render_markdown_image_uris(sender, instance, using, **kwargs):
     """Replace EventImage identifiers with real URIs in submitted markdown."""
     event = Event.objects.get(id=instance.event_id)
     event.render_markdown_uris()
-
-
-@receiver(post_delete, sender=EventImage)
-def delete_image_file(sender, instance, using, **kwargs):
-    """Delete image file if it exists."""
-    if os.path.isfile(instance.file.path):
-        os.remove(instance.file.path)
-
-
-@receiver(pre_save, sender=EventImage)
-def delete_old_image_file(sender, instance, using, **kwargs):
-    """Delete old image file if it exists."""
-    if not instance.id:
-        return False
-
-    try:
-        old_image = EventImage.objects.filter(id=instance.id)
-    except EventImage.DoesNotExist:
-        return False
-
-    if (
-        old_image.file != instance.file
-        and os.path.isfile(old_image.file)
-    ):
-        old_image.file.delete(save=False)
