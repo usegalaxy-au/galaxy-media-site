4a19cd69e29b2803f746376fdab72b8787c267f7
Cameron Hyde <chyde@neoformit.com>
2023-08-14 16:05:23 +1000

    Add text search for matrices

--- webapp/home/static/home/js/tree-list.js
+++ webapp/home/static/home/js/tree-list.js
@@ -1,13 +1,70 @@
-// TODO: on load, set value of terms statement
-// TODO: on load, set value of matrices
-// TODO: scroll to backend errors on load
-
 $(document).ready(function() {
   setMatricesValue();
   setTermsValue();
   addTreeBranchClickEvent();
+  scrollToErrors();
+  // Add event handler to update "selected matrices" count
   $('#taxonomy-tree input[type="checkbox"]').change(updateTreeSelection);
 });
+
+const debouncedSearchTree = (q) => _.debounce(
+  // Debounce to prevent rapid firing while user types
+  () => searchTree(q),
+  500,
+  { 'leading': true, 'trailing': false },
+)();
+
+function searchTree(query) {
+  resetTreeList();
+  if (!query) return;
+  let exit = false;
+  const matched = [];
+  const choices = $('#taxonomy-tree span.choice');
+  choices.each( (i, node) => {
+    if (exit) return;
+    const text = $(node).find('label').text();
+    if (text.toLowerCase().indexOf(query.toLowerCase()) > -1) {
+      const input_id = $(node).find('input').val();
+      matched.push({node: node, label: text, id: input_id});
+    }
+  });
+  showTreeMatches(matched);
+}
+
+function showTreeMatches(matched) {
+  dropdown = $('#treeSearch .dropdown');
+  dropdown.empty();
+  matched.forEach( (match) => {
+    const option = $(`
+      <option onclick="selectTreeMatch('${match.id}');">
+        ${match.label}
+      </option>`);
+    dropdown.append(option);
+  });
+  // Add click handler to body to close dropdown when clicking outside treeSearch
+  $('body').click( (e) => {
+    if ( !$(e.target).closest('#treeSearch').length) {
+      dropdown.empty();
+    }
+  });
+}
+
+function selectTreeMatch(id) {
+  $('#treeSearch .dropdown').empty();
+  let parent = $(`#taxonomy-tree input[value="${id}"]`).closest('ul');
+  while (parent.hasClass('nested')) {
+    parent.siblings('.caret').click();
+    parent = $(parent).parent().parent();
+  }
+}
+
+function scrollToErrors() {
+  const errors = $('.errorlist');
+  if (errors.length) {
+    const top = errors.offset().top;
+    $('html, body').animate({scrollTop: top - 80}, 500);
+  }
+}
 
 function addTreeBranchClickEvent() {
   let i;
@@ -53,8 +110,9 @@ function updateTreeSelection() {
 }
 
 function clearTreeSelection() {
-    $('#taxonomy-tree input[type="checkbox"]:checked').prop('checked', false);
-    updateTreeSelection();
+  $('#treeSearch input').val('');
+  $('#taxonomy-tree input[type="checkbox"]:checked').prop('checked', false);
+  updateTreeSelection();
 }
 
 function resetTreeList() {
