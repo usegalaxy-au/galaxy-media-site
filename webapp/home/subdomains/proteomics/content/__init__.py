20f08a1ee7b93093ba98a5335f3b3b47859ff03a
Johan Gustafsson <johan@biocommons.org.au>
2023-10-23 15:18:06 +1030

    Working group feedback for Proteomics Lab sections

--- webapp/home/subdomains/proteomics/content/__init__.py
+++ webapp/home/subdomains/proteomics/content/__init__.py
@@ -1,5 +1,7 @@
 from . import (
     data,
+    conversion_modification,
+    database_searching,
     post_processing_visualisation,
     quant_proteomics,
 )
---
+++ webapp/home/subdomains/proteomics/content/database_searching.py
@@ -0,0 +1,87 @@
+"""Content for data section - tools, workflows and help tabs."""
+
+galaxy_au_support_item = {
+    "title_html": 'Galaxy Australia support',
+    "description_html": """<p>
+            Any user of Galaxy Australia can request support through an
+            online form.
+        </p>""",
+    "button_link": "/request/support",
+    "button_html": "Request support",
+}
+
+tools = [
+    # {   # Accordion item schema:
+    #     "title_html": '',
+    #     "description_html": """""",
+    #     "inputs": [
+    #         {
+    #             'datatypes': [''],
+    #             'label': '',
+    #         },
+    #     ],
+    #     "button_link": "",
+    #     "button_html": "",
+    #     "button_tip": "",
+    #     "view_link": "",
+    #     "view_html": "",
+    #     "view_tip": "",
+    # },
+    {
+        "title_html": '<code>DecoyDatabase</code>',
+        "description_html": '<p>Create decoy sequence database from forward sequence database.</p>',
+        "inputs": [
+            {
+                'datatypes': ['fasta'],
+                'label': 'Input FASTA file(s), each containing a database',
+            }
+        ],
+        "button_link": "https://proteomics.usegalaxy.org.au/root?tool_id=toolshed.g2.bx.psu.edu/repos/galaxyp/openms_decoydatabase/DecoyDatabase/",
+    },
+    {
+        "title_html": '<code>MaxQuant</code>',
+        "description_html": '<p>MaxQuant is a quantitative proteomics software package designed for analyzing large mass-spectrometric data sets.</p>',
+        "inputs": [
+            {'datatypes': ['thermo.raw']},
+            {'datatypes': ['mzML']},
+            {'datatypes': ['mzXML']},
+            {
+                'datatypes': ['tabular'],
+                'label': 'Experimental design template',
+            },
+        ],
+        "button_link": "https://usegalaxy.org.au/root?tool_id=toolshed.g2.bx.psu.edu/repos/galaxyp/maxquant/maxquant/",
+    },
+    {
+        "title_html": '<code>Morpheus</code>',
+        "description_html": '<p>Database search algorithm for high-resolution tandem mass spectra.</p>',
+        "inputs": [
+            {
+                'datatypes': ['mzml'],
+                'label': 'Indexed mzML',
+            },
+            {
+                'datatypes': ['fasta,uniprotxml'],
+                'label': 'MS Protein Search Database: UniProt Xml or Fasta',
+            },
+        ],
+        "button_link": "https://proteomics.usegalaxy.org.au/root?tool_id=toolshed.g2.bx.psu.edu/repos/galaxyp/morpheus/morpheus/",
+    },
+    {
+        "title_html": '<code>MSFraggerAdapter</code>',
+        "description_html": '<p>Peptide Identification with MSFragger.</p>',
+        "inputs": [
+            {
+                'datatypes': ['mzml,mzxml'],
+                'label': 'Input File with spectra for MSFragger',
+            },
+            {
+                'datatypes': ['fasta'],
+                'label': 'Protein FASTA database file path',
+            },
+        ],
+        "button_link": "https://proteomics.usegalaxy.org.au/root?tool_id=toolshed.g2.bx.psu.edu/repos/galaxyp/openms_msfraggeradapter/MSFraggerAdapter/",
+    }
+]
+
+help = []  # Todo
