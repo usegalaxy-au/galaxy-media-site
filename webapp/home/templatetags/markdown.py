bbcf3e671b5697e0e951c2a4f01717d4780316f0
Cameron Hyde <chyde@neoformit.com>
2024-07-08 09:49:28 +1000

    Labs template markdown from remote url

--- webapp/home/templatetags/markdown.py
+++ webapp/home/templatetags/markdown.py
@@ -4,7 +4,10 @@ https://github.com/trentm/python-markdown2
 """
 
 import markdown2
+import re
+import requests
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -22,3 +25,28 @@ def markdown(md):
         },
     })
     return html
+
+
+@register.simple_tag()
+def markdown_from_url(url):
+    """Fetch content from URL and render html from markdown string."""
+    body = requests.get(url).text
+
+    # Remove YAML header if present
+    liquid_md = (
+        body.rsplit('---\n', 1)[1]
+        if '---\n' in body
+        else body
+    )
+    # remove Liquid syntax {: ... } strings
+    md = re.sub(r'\{:.*\}', '', liquid_md)
+
+    html = markdown2.markdown(md, extras={
+        "tables": True,
+        "code-friendly": False,
+        "html-classes": {
+            'table': 'table table-striped',
+        },
+    })
+
+    return mark_safe(html)
