1bbd00bc90108a8959f04c7463820e260660313c
Cameron Hyde <chyde@neoformit.com>
2025-01-10 07:08:30 +1000

    Symlink maintenance static files

--- deploy/maintenance-site/static/home/js/admin-mde.js
+++
@@ -1,9 +0,0 @@
-// Add markdown editor to the "body" field in django admin
-
-(function($) {
-  $(document).ready( () => {
-    var simplemde = new SimpleMDE({
-      element: document.getElementById("id_body")
-    });
-  });
-})(django.jQuery);
