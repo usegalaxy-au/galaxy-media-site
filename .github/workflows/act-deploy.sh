a52f42a39de9093d7a5eaf1b5f8bbb4e2a9b1da6
Cameron Hyde <chyde@neoformit.com>
2025-03-21 18:07:38 +1000

    Deploy workflows tested/working with act

--- .github/workflows/act-deploy.sh
+++ .github/workflows/act-deploy.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-HOSTNAME=dev-site.gvl.org.au
+HOSTNAME=site.usegalaxy.org.au
 
 export ACT_RUNNER=ubuntu-latest
 
+source "$(dirname $0)/../../venv/bin/activate"
+
 gh act workflow_dispatch \
-    -W .github/workflows/deploy-dev.yml \
-    -s ANSIBLE_VAULT_PASS="$ANSIBLE_VAULT_PASS" \
+    -W .github/workflows/deploy.yml \
+    -s ANSIBLE_VAULT_PASS_BASE64="$ANSIBLE_VAULT_PASS_BASE64" \
     -s SSH_PRIVATE_KEY="$(cat ~/.ssh/github)" \
-    -s SSH_KNOWN_HOSTS="$(ssh-keyscan $HOSTNAME)" \
     -s SSH_USER="ubuntu" \
     --container-architecture linux/amd64
